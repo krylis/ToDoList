bd0860ea56c9296612acfd68faa84df4504dfc7d
krylis <cmcshann@gmail.com>
2023-08-22 19:49:04 -0700

    Tasks can now be deleted and marked as complete

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ db.init_app(app)
 
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String, unique=True, nullable=False)
+    task = db.Column(db.String, nullable=False)
     due_date = db.Column(db.DateTime, nullable=False)
     complete = db.Column(db.Boolean, default=False)
 
@@ -29,8 +29,30 @@ def index():
 @app.route('/add', methods=["POST"])
 def add_task():
     task = request.form['task']
-    date = datetime.strptime(request.form['date'], '%Y-%m-%d')
+    date_str = request.form['date']
+    if date_str == '':
+        date = datetime.utcnow()
+    else:
+        date = datetime.strptime(request.form['date'], '%Y-%m-%d')
     db.session.add(Tasks(task=task, due_date=date))
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    task = Tasks.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+@app.route('/complete/<int:task_id>')
+def complete_task(task_id):
+    task = Tasks.query.get_or_404(task_id)
+    task.complete = True
     db.session.commit()
 
     return redirect(url_for('index'))
