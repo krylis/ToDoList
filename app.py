5dc1ec53f61e400d20b946e641ff01488c267a3e
krylis <cmcshann@gmail.com>
2023-08-22 19:24:38 -0700

    Tasks are now saved to and pulled from an sqlite database

--- app.py
+++ app.py
@@ -1,32 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///tasks.db"
+
+db = SQLAlchemy()
+db.init_app(app)
 
 
-class Task:
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String, unique=True, nullable=False)
+    due_date = db.Column(db.DateTime, nullable=False)
+    complete = db.Column(db.Boolean, default=False)
 
-    def __init__(self, task, due_date):
-        self.task = task
-        self.due_date = due_date
-        self.complete = False
 
-
-my_tasks = []
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def index():
+    my_tasks = Tasks.query.order_by(Tasks.due_date)
     return render_template("index.html", tasks=my_tasks)
 
 
 @app.route('/add', methods=["POST"])
 def add_task():
     task = request.form['task']
-    date = request.form['date']
-    new_task = Task(task, date)
-    my_tasks.append(new_task)
-    my_tasks.sort(key=lambda x: x.due_date)
+    date = datetime.strptime(request.form['date'], '%Y-%m-%d')
+    db.session.add(Tasks(task=task, due_date=date))
+    db.session.commit()
 
     return redirect(url_for('index'))
 
